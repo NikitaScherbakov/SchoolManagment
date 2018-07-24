b7a5c92c07f8f9a0e28b71d33fb82035c29d1888
Никита <layzendango@gmail.com>
2018-07-24 11:37:43 +0300

    Add project files.

---
+++ School.DAL/Entities/SchoolClass.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace School.DAL.Entities
+{
+    public class SchoolClass
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        
+        public ICollection<Pupil> Pupils { get; set; }
+
+        public ICollection<Teacher> Teachers { get; set; }
+
+        public SchoolClass()
+        {
+            Teachers = new List<Teacher>();
+        }
+    } 
+}
---
+++ School.DAL/Entities/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace School.DAL.Entities
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<SchoolClass> SchoolClasses { get; set; }
+        public Teacher()
+        {
+            SchoolClasses = new List<SchoolClass>();
+        }
+    }
+}
